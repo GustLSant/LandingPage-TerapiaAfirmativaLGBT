6e8caf1d6a2de4ee8286b6e5669a0ccdc4daaf8b
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2023-04-09 21:18:03 -0300

    fru-frus e funcoes js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function toggleQuestion(id){
     document.getElementById(id).classList.toggle('active')
-    console.log("asd")
+}
+
+
+function redirectMozer(){
+    window.open('https://www.instagram.com/mozerbeier/','_blank')
+}
+
+
+function redirecionarWpp(){
+    window.open('https://wa.me/557583687425', '_blank')
+}
+
+
+function onClickComprarCurso(){
+    const sectionComprar = document.getElementById("section-comprar")
+    const offset = sectionComprar.getBoundingClientRect().top - document.body.getBoundingClientRect().top + 150
+    window.scrollTo({top: offset, behavior: 'smooth'})
 }
